a15dc17e97c4f8250b330d47f3411ca54ebb6fe1
WORK <andi.hidayat.mail@gmail.com>
2020-11-22 19:47:30 +0700

    scrapping detikcom

--- detik_scrapper.py
+++ detik_scrapper.py
@@ -3,3 +3,11 @@ from bs4 import BeautifulSoup
 
 html_doc = requests.get('https://www.detik.com/terpopuler', params={'tag_from': 'wp_cb_mostPopular_more'})
 print(html_doc.text)
+
+soup = BeautifulSoup(html_doc.text, 'html.parser')
+populer_area = soup.find(attrs={'class': 'list-content'})
+titles = populer_area.find_all(attrs={'class': 'media_title'})
+
+for title in titles:
+    print(title.text)
+# print(titles)
