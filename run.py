58cc7c8a1d5732e49ca5566983c32f7f518b3889
WORK <andi.hidayat.mail@gmail.com>
2020-11-22 21:28:15 +0700

    scrapping detikcom

--- run.py
+++ run.py
@@ -5,15 +5,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
- 
+
+@app.route('\detik-populer')
+def detik_populer():
+    html_doc = requests.get('https://www.detik.com/terpopuler', params={'tag_from': 'wp_cb_mostPopular_more'})
+    print(html_doc.text)
+
+    soup = BeautifulSoup(html_doc.text, 'html.parser')
+    populer_area = soup.find(attrs={'class': 'list-content'})
+    titles = populer_area.find_all(attrs={'class': 'media_title'})
+    images = populer_area.find_all(attrs={'class': 'media_image'})
+
+    return render_template('index.html', images=images)
 
 
-
-
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
